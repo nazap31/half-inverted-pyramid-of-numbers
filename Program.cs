cdd067905148504fdbc27fecdb67934101ba3302
nazap31 <you@example.com>
2022-06-23 00:23:07 -0600

    media-piramide-invertida

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//piramide invertida
+
+Console.Write("Ingrese un numero: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+for (int i = numero; i >= 1; i--)
+{
+    for (int j = i; j >= 1; j--)
+    {
+        Console.Write(j + " ");
+    }
+        Console.Write("\n");
+}
+Console.WriteLine("Programa terminado!");
+
+
+
